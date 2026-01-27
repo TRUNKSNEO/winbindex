1d7a9b41d46752c78770a6977296f1135fa09fdd
Michael Maltsev <4129781+m417z@users.noreply.github.com>
2026-01-27 11:37:34 +0200

    Delete Windows Server only updates: KB5070881, KB5072359
    These updates are no longer listed.

--- data/config.py
+++ data/config.py
@@ -16,10 +16,8 @@ updates_unsupported = {
     'KB5016139',
 
     # Windows Server only.
-    'KB5070881',
     'KB5070882',
     'KB5070883',
-    'KB5072359',
 
     # Missing on the Update Catalog.
     'KB5071959',
